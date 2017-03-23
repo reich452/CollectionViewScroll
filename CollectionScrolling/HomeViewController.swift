588de31c138c70f8bd9c2f03e82e792b1b7726e5
reich452 <reich452@regis.edu>
2017-03-23 14:15:49 -0600

    Added images

---
+++ CollectionScrolling/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  CollectionScrolling
+//
+//  Created by Nick Reichard on 3/23/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+
+}
